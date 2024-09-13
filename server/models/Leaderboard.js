524a2fe4e44f73285c726463c070a472a754a1b9
Johannes Borg <johannes.borg03@gmail.com>
2024-09-13 13:44:53 +0200

    #1 Add leaderboard file and schema fields

---
+++ server/models/Leaderboard.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var leaderboardSchema = mongoose.Schema({
+    leaderboard_id : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    month : {
+        type String,
+        required : true,
+        min : 3,
+        max : 20
+    }
+});
+
+var Leaderboard = mongoose.model('Leaderboard', leaderboardSchema);
