685fda728e74c18e4e984a35cf88468d45a71a8e
Ravi <ravisha@chalmers.se>
2024-09-17 02:16:04 +0200

    Add CRUD operations for activities

--- server/models/Activities.js
+++ server/models/Activities.js
@@ -1,20 +1,92 @@
-const activity = require('./Activity');
-var activityController = require('./controllers/activities');
+var express = require('express');
+var router = express.router();
 
+var Activity = require('../models/Activity.js');
 
-app.post('/api/activities', (req, res) => {
+router.post('/api/activities', async function (req, res) {
     // Instantiate the Activity model with request data
     var new_activity = new Activity({
-        "activity_id": req.body.activity_id, // If you want to set the ID manually (unusual), otherwise omit this
-        "beercount": req.body.beercount,
-        "activity_type": req.body.activity_type,
-        "user": req.body.user
+        activity_id: req.body.activity_id,
+        beercount: req.body.beercount,
+        activity_type: req.body.activity_type,
+        user: req.body.user
     });
 
+    try {
+        await user.save();
+
+    } catch (err) {
+        next(err);
+    }
+    3
     res.status(201).json(new_activity);
 });
 
-
-app.use('/api/*', function (req, res) {
-    res.status(404).json({ 'message': 'Not Found' });
+// Get all activities
+router.get('/api/activities', async function (req, res,) {
+    const activities = await Activity.find();
+    res.json({ 'activities': activities });
 });
+
+//Get specific activity
+router.get('/api/activities/:activity_id', async function (req, res) {
+    try {
+        var activityID = req.params.activity_id;
+        const activity = await Activity.findOne({ activity_id: activityID });
+        if (!activity) {
+            return res.status(404).json({ "message": "No such activity" });
+        }
+        res.json(activity);
+    } catch (err) {
+        res.status(500).json({ "message": "Server error", "error": err.message });
+    }
+});
+
+//Update a activity
+router.put('/api/activities/:activity_id', async function (req, res) {
+    try {
+        var activityID = req.params.activity_id;
+        const updatedData = req.body;
+        const updatedActivity = await Activity.findOneAndUpdate({ activity_id: activityID }, updatedData,
+            { new: true, runValidators: true }
+        );
+        if (!updatedActivity) {
+            return res.status(404).json({ "message": "No such activity" });
+        }
+        res.json(updatedActivity);
+    } catch (err) {
+        res.status(500).json({ "message": "Server error", "error": err.message });
+    }
+});
+
+// Delete a specific activity 
+router.delete('/api/activities/:activity_id', async function (req, res) {
+    try {
+        var activityID = req.params.activity_id;
+        const deletedActivity = await Activity.findOneAndDelete({ activity_id : activityID });
+        if (!deletedActivity) {
+            return res.status(404).json({ "message": "No such activity" });
+        }
+        res.json({ "message": "Activity deleted successfully", "activity": deletedActivity });
+    } catch (err) {
+        res.status(500).json({ "message": "Server error", "error": err.message });
+    }
+});
+
+//Delete all activities
+router.delete('/api/activities', async function (req, res) {
+    try {
+        const deletedResult = await Activity.deleteMany({});
+        res.status(200).json({
+            message: "All activities successfully deleted",
+            deletedCount: deletedResult.deletedCount,
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: "Server error while deleting activities",
+            error: err.message,
+        });
+    }
+});
+
+
