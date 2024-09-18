b4974ae5314aeff3a13d47ce2970e23ebad63a61
Johannes Borg <johannes.borg03@gmail.com>
2024-09-18 18:04:21 +0200

    #7 Add activity log POST API call successfully

--- server/controllers/Users.js
+++ server/controllers/Users.js
@@ -2,10 +2,10 @@ var express = require ('express');
 var router = express.Router();
 
 var User = require('../models/User.js');
-
+var Activity = require('../models/Activity.js');
 
 // Create a new user (POST /api/users)
-router.post('/api/Users', async function (req, res, next) {
+router.post('/api/users', async function (req, res, next) {
     var user = new User({
         userID: req.body.userID,
         username: req.body.username,
@@ -111,6 +111,55 @@ router.patch('/api/users/:userID', async function (req, res) {
         });
     }
 });
+
+// Create a POST route for logging Activity for a specific User
+router.post('/api/users/:userID/activities', async function (req, res) {
+    try {
+        const user = await User.findOne({ userID : req.params.userID });
+        
+        if (!user) {
+            return res.status(404).json({ message : "User not found"});
+        }
+
+        //Create the new Activity
+        const newActivity = new Activity ({
+            activity_id : req.body.activity_id,
+            beercount : req.body.beercount,
+            activity_type : req.body.activity_type,
+            user : user._id //Can you do this?
+        });
+
+        //Save Activity
+        const savedActivity = await newActivity.save();
+
+        //Push the activity into the Users Activities array
+        user.activities.push(savedActivity._id)  //Again, can you do this with the id??
+        await user.save();
+
+        res.status(201).json({
+            message : "Activity logged Successfully",
+            activity : savedActivity
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message : "Server error while logging activity",
+            error : error.message
+        });
+    }
+
+});
+
+
+
+// Create a GET route for Activities for a specific User
+
+// Create a GET route for a specifc Activity for a Specific User
+
+// Create a DELETE route for a specific Activity for a Specific User
+
+//OPTIONAL: Create a DELETE ROUTE for All activities for a Specific User
+
 
 
 
