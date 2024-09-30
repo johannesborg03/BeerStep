75060581a6956c24daea2222b17631ea2d914526
Ravi <ravisha@chalmers.se>
2024-09-30 23:55:56 +0200

    #22 Add styles to Navbar

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -20,5 +20,67 @@ export default {
 </script>
 
 <style>
+header {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  height: 80px;
+  width: 100%;
+  background: #333;
+  display: flex;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
 
+header h1 {
+  flex: 1;
+  font-size: 40px;
+  text-align: center;
+  margin: 0;
+  color: #ebb112;
+  margin-left: 40px;
+}
+
+.user {
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+}
+
+.profile-pic {
+  width: 35px;
+  height: 35px;
+  border-radius: 50%;
+  overflow: hidden;
+  margin-right: 10px;
+}
+
+.profile-pic img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.settings {
+  width: 35px;
+  height: 35px;
+  border-radius: 50%;
+  margin-left: 100px;
+}
+
+.settings img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  
+}
+
+.settings:hover {
+  transform: scale(1.1);
+  opacity: 0.8;
+}
+
+.username {
+  font-size: 16px;
+  color: #ffffff;
+  margin-right: 100px;
+  margin-top: 9px;
+}
 </style>
