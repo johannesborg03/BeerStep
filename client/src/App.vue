993003406ad061db5befb8f2932def1efa99bf69
Ravi <ravisha@chalmers.se>
2024-09-30 23:54:56 +0200

    #22 Create structure for template

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,29 @@
+<!-- src/App.vue -->
 <template>
   <div id="app">
+    <!-- Include the Navbar component here -->
+    <Navbar />
+
     <div id="nav">
       <router-link to="/">Home</router-link>
     </div>
+
     <!-- Render the content of the current page view -->
-    <router-view/>
+    <router-view />
   </div>
 </template>
+
+<script>
+// Import the Navbar component
+import Navbar from './components/Navbar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Navbar, // Register Navbar as a component in App.vue
+  },
+};
+</script>
 
 <style>
 #app {
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <div class="settings">
+      <img src="/src/assets/settings.png" alt="Settings" />
+    </div>
+    <h1>BeerStep</h1>
+    <div class="user">
+      <div class="profile-pic">
+        <img src="/src/assets/userPic.png" alt="User profile picture" />
+      </div>
+      <p class="username">Jabo</p>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
+
+<style>
+
+</style>
