ff6afea384bf5424af9bf99559da28ab0cebde78
Johannes Borg <johannes.borg03@gmail.com>
2024-10-01 16:30:59 +0200

    #18 Add css hover and background color for buttons

--- client/src/views/Activity.vue
+++ client/src/views/Activity.vue
@@ -4,8 +4,8 @@
       <!-- Add the content you want to display in the view -->
       <p>This is where the activity data will appear.</p>
       <div class="button-container">
-        <button class="massive-button log-beer">Beer</button>
-        <button class="massive-button log-beer">Log Steps</button>
+        <button class="massive-button beer">Beer</button>
+        <button class="massive-button log-step">Log Steps</button>
       </div>
     </div>
   </template>
@@ -39,6 +39,26 @@ export default {
     border-radius: 10px; /* rounded corners */
     cursor: pointer; /* Change cursor to pointer on hover */
     transition: transform 0.3s ease, background-color 0.3s ease; /* Smooth transitions */
-
 }
+
+.beer {
+    background-color: #28a745;
+}
+
+.beer:hover {
+transform: translateY(-50px);
+background-color: #218838; /* Darker shade on hover */
+transform: scale(1.2); /* Slightly enlarge on hover */
+}
+
+.log-step {
+    background-color: #007bff;
+}
+
+.log-step:hover {
+transform: translateY(-50px);
+background-color: #0056b3; /* Darker shade on hover */
+transform: scale(1.2); /* Slightly enlarge on hover */
+}
+
 </style>
