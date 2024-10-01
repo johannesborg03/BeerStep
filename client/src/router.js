e1f72ffe29113c171f7b85a3b12ebb1e6235faf8
Johannes Borg <johannes.borg03@gmail.com>
2024-10-01 15:53:40 +0200

    #18 Add route for activity

--- client/src/router.js
+++ client/src/router.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from './views/Home.vue'
+import Activity from '@/views/Activity.vue'
 
 const routes = [
   { path: '/', name: 'Squad', component: Home },
   { path: '/', name: 'Log activity', component: Home },
-  { path: '/', name: 'Leaderboard', component: Home }
+  { path: '/', name: 'Leaderboard', component: Home },
+  { path: '/', name: 'Activity', component: Activity }
 ]
 
 const router = createRouter({
