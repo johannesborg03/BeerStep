9ddd0210db016caba16957dd56fa99654be5a53b
Ravi <ravisha@chalmers.se>
2024-10-01 23:54:28 +0200

    #23 Update homepage  layout to align with About Us page design

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,117 +1,114 @@
 <template>
-  <header>
-    <div class="settings" @click="toggleMenu">
-      <img src="/src/assets/settings.png" alt="Settings" />
-      <div v-if="menuVisible" class="menu">
-      <ul>
-        <li @click="goToSettings">Settings</li>
-        <li @click="logout">Logout</li>
-      </ul>
-    </div>
-    </div>
-    <h1>BeerStep</h1>
-    <div class="user">
-      <div class="profile-pic">
-        <img src="/src/assets/userPic.png" alt="User profile picture" />
+
+  <body>
+    <main>
+      <div class="beerBack">
+        <img src="/src/assets/beerBackground.jpg">
+        <router-link to="/">Home</router-link>
+        <div class="button-container">
+          <router-link to="/log-activity" class="button red">Log activity</router-link>
+        </div>
       </div>
-      <p class="username">Jabo</p>
-    </div>
-  
-  
-  
-  </header>
- </template>
- <script>
- export default {
-  name: 'Navbar',
+
+    </main>
+  </body>
+</template>
+
+<script>
+// @ is an alias to /src
+import { Api } from '@/Api'
+
+export default {
+  name: 'home',
   data() {
     return {
-      menuVisible: false
-    };
+      message: 'none'
+    }
   },
   methods: {
-    toggleMenu() {
-      this.menuVisible = !this.menuVisible;
-    },
-    goToSettings() {
-      this.$router.push('/Settings');
-    },
-    logout() {
-      this.$router.push('/LogIn');
+    getMessage() {
+      Api.get('/')
+        .then(response => {
+          this.message = response.data.message
+        })
+        .catch(error => {
+          this.message = error
+        })
     }
   }
- };
- </script>
- <style>
- header {
+}
+</script>
+
+<style>
+/* General */
+body {
   font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-  height: 80px;
-  width: 100%;
-  background: #333;
-  display: flex;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  position: relative;
-  /* Added for dropdown positioning */
- }
- header h1 {
-  flex: 1;
-  font-size: 44px;
-  text-align: center;
+  background: #2b2b2b;
+  color: #f1f1f1;
   margin: 0;
-  color: #ebb112;
-  margin-left: 40px;
-  margin-top: 10px;
-  font-family: 'Segoe UI';
- }
- .user {
-  display: flex;
-  align-items: center;
-  margin-left: auto;
- }
- .profile-pic {
-  width: 35px;
-  height: 35px;
-  border-radius: 50%;
-  overflow: hidden;
-  margin-right: 10px;
- }
- .profile-pic img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
- }
- .settings {
-  width: 35px;
-  height: 35px;
-  border-radius: 50%;
-  margin-left: 100px;
-  margin-top: 20px;
-  cursor: pointer;
- }
- .settings img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
- }
- .username {
-  font-size: 16px;
-  color: #ffffff;
-  margin-right: 100px;
-  margin-top: 9px;
- }
- .menu {
-  top: 80px;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
- }
- .menu ul {
-  list-style: none;
   padding: 0;
-  margin: 0;
-  z-index: 10;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+}
+
+/* Main */
+main {
+  position: relative;
+  width: 100%;
+  display: flex;
+  /* Enable flexbox layout for the main section */
+  flex-direction: column;
+  /* Ensure main content flows vertically */
+  align-items: center;
+  /* Center align items */
+
+}
+
+/* Beer Background */
+.beerBack {
+  border-radius: 3%;
+  overflow: hidden;
+  width: 91vw;
+  height: 70vh;
+  margin: 0 auto;
+  position: relative;
+}
+
+.beerBack img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.button-container {
+  display: flex;
+  width: 30%;
   position: absolute;
- }
- .menu li {
-  padding: 10px 20px;
- }
- </style>
- 
+  top: 60%;
+  /* Move down on the page */
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding-bottom: 80px;
+}
+
+.button {
+  margin-top: 500px;
+  padding: 4px 0;
+  font-size: 17px;
+  color: #f2f1ea;
+  border-radius: 15px;
+  text-align: center;
+  text-decoration: none;
+  width: 60px;
+  height: 35px;
+  flex: 1;
+  transition: 0.3s;
+  background: #a62b23;
+}
+
+.button:hover {
+  transform: translateY(-3px);
+}
+</style>
