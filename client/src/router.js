0e2a2e6c2905fb8eed1c66ded5b690ec78ed9752
Erik Nisbet <eriknis@student.chalmers.se>
2024-10-02 13:31:54 +0200

    #20 Add styling to Leaderboard.vue and add router path in router.js

--- client/src/router.js
+++ client/src/router.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from './views/Home.vue'
+import Leaderboard from './components/Leaderboard.vue'
 
 const routes = [
   { path: '/', name: 'Squad', component: Home },
   { path: '/', name: 'Log activity', component: Home },
-  { path: '/', name: 'Leaderboard', component: Home }
+  { path: '/leaderboard', name: 'Leaderboard', component: Leaderboard }
 ]
 
 const router = createRouter({
