41399c6f5967f9603d6e73e80cdc8032e5537d51
Johannes Borg <johannes.borg03@gmail.com>
2024-10-07 15:22:48 +0200

    #27 Add methods for logging beer and logging steps in Activity model

--- server/models/Activity.js
+++ server/models/Activity.js
@@ -28,6 +28,23 @@ var activitySchema = mongoose.Schema({
     }
 });
 
+// Method for logging a beer and automatically adding 2000 steps to steps needed
+activitySchema.methods.logBeer = function(beerCount) {
+    this.total_beers += beerCount;
+    this.steps_needed += beerCount * 2000;
+    return this.save();
+};
+
+activitySchema.methods.logSteps = function(steps) {
+    this.total_steps += steps;
+    if(this.steps_needed > 0) {
+        this.steps_needed = Math.max(0, this.steps_needed - steps);
+    }
+    return this.save();
+}
+
+
+
 var Activity = mongoose.model('activities', activitySchema);
 
 module.exports = Activity;
