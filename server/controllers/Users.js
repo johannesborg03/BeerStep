638a492ae926d0f7e7be2ee9c55a63a4ed422bf6
Johannes Borg <johannes.borg03@gmail.com>
2024-10-07 11:39:57 +0200

    29 Change Get route for specific activity for user to username instead of UserId)

--- server/controllers/Users.js
+++ server/controllers/Users.js
@@ -243,9 +243,9 @@ router.get('/api/users/:username/activities', async function (req, res) {
 });
 
 // Create a GET route for a specifc Activity for a Specific User
-router.get('/api/users/:userID/activities/:activityId', async function (req, res){
+router.get('/api/users/:username/activities/:activityId', async function (req, res){
     try {
-        const user = await User.findOne({userID : req.params.userID});
+        const user = await User.findOne({username : req.params.username});
 
         if (!user){
             return res.status(404).json({ message : "User not found"});
