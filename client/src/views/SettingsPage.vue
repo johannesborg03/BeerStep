4a27ca5dda1126dbcf72ce42942c17b13368cb52
Johannes Borg <johannes.borg03@gmail.com>
2024-10-09 09:41:44 +0200

    #19 Add settings component and header in settingspage

---
+++ client/src/views/SettingsPage.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <body>
+        <header>
+            BeerStep
+        </header>
+    <main>
+        
+    <div class="settings-view">
+
+    </div>
+</main>
+</body>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
