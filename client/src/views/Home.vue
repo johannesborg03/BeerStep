2cd9a647b712152ea612e74808f7ffb25e8499e4
Ravi <ravisha@chalmers.se>
2024-10-12 18:30:26 +0200

    #38 Add boostrap structure

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,29 @@
 <template>
-  <main>
-    <div class="content">
-      <h1 class="main-title">BeerStep</h1>
-    </div>
-    <div class="login-wrapper">
-      <router-link to="/LogIn" class="buttonNav">Log in</router-link>
-      <b-container class="d-flex justify-content-center align-items-center vh-100"></b-container>
-    </div>
-    <b-container class="subtitle mt-10">
-      <h1 class="headline">Healthy drinking, made easy with BeerStep</h1>
-      <p class="subtext">Join BeerStep and balance fun with wellness!</p>
-    </b-container>
-  </main>
+  
+  <BContainer fluid>
+  <BRow class="align-items-center">
+
+    <!-- Empty column for left side -->
+    <BCol></BCol>
+    
+    <BCol class="text-center">
+      <h1 class="main-title display-3">BeerStep</h1>
+    </BCol>
+    
+    <BCol class="text-end">
+      <router-link to="/LogIn">
+        <BButton variant="warning">Log In</BButton>
+      </router-link>
+    </BCol>
+  </BRow>
+</BContainer>
+  
+    <BRow class="textArea text-start">
+      <BCol>
+        <h1 class="headline display-4 mb-3">Healthy drinking, made easy with BeerStep</h1>
+        <p class="subtext lead">Join BeerStep and balance fun with wellness!</p>
+      </BCol>
+    </BRow>
 </template>
 
 
@@ -64,64 +76,11 @@ export default {
     text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5);
   }
 }
-main {
-    position: relative;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
 
-.login-wrapper { 
-    background-image: url('@/assets/aboutImg.jpg');
-    background-size: cover;
-    background-position: center;
-    height: 100%;
-    width: 100%;
-}
 
-/* Center the content over the image */
-.content {
-    position: absolute;
-    text-align: center;
-    color: white;
-    left: 50%;
-    margin-top: 50px;
-    transform: translate(-50%, -50%);
-    position: absolute;
-    text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5);
-    margin-right: 15px;
+.textArea{
 
 }
 
-.buttonNav {
-    padding: 10px;
-    position: absolute;
-    right: 30px; 
-    margin-top: 40px;
-    transform: translateY(-50%);
-}
 
-.buttonNav,
-router-link {
-    color: white;
-    text-decoration: none;
-    font-size: 18px;
-    text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5);
-}
-
-.subtitle {
-    position: absolute;
-    text-align: left;
-    color: white;
-    font-family: 'Segoe UI';
-    margin-top: 200px;
-    max-width: 450px;
-    left: 20px;
-    text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5);
-    padding-left: 50px;
-    font-size: 28px;
-    
-}
 </style>
